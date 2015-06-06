10a5e1ae2c9ac727e6a9f6c989c9a2f9c5cf0b98
Jesús Lara <jlc00016@gmail.com>
2015-06-06 20:08:37 +0200

    Creada sección creditos en la página

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,16 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/author', function(reg, res) {
+  res.render('author',
+      { authors: [{ name: 'Jesús Lara (jlc00016)',
+                    urlPhoto: '/images/miFoto.jpg',
+                    urlVideo: 'http://techslides.com/demos/sample-videos/small.mp4'
+                  }]
+      }
+  );
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer',   quizController.answer);
 
