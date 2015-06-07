1b2f6ea3f57afd2e8fd7580a8dee6c6a4b0eacb4
Jesús Lara <jlc00016@gmail.com>
2015-06-07 18:14:47 +0200

    Lista de preguntas

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,28 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+// Página de entrada (home page)
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/author', function(reg, res) {
-  res.render('author',
-      { authors: [{ name: 'Jesús Lara (jlc00016)',
-                    urlPhoto: '/images/miFoto.jpg',
-                    urlVideo: 'http://techslides.com/demos/sample-videos/small.mp4'
-                  }]
-      }
-  );
+// Pagina o seccion autor
+router.get('/author', function (reg, res) {
+    res.render('author',
+        {
+            authors: [{
+                name: 'Jesús Lara (jlc00016)',
+                urlPhoto: '/images/miFoto.jpg',
+                urlVideo: 'http://techslides.com/demos/sample-videos/small.mp4'
+            }]
+        }
+    );
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer',   quizController.answer);
+// Definición de rutas de /quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 
 module.exports = router;
