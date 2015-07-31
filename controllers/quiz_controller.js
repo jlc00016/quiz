126a5fd9ddccc7698245ff4783fa287d6ef1b28a
Jesús Lara <jlc00016@gmail.com>
2015-07-31 19:32:19 +0200

    Borrar pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -59,20 +59,23 @@ exports.new = function (req, res) {
 exports.create = function (req, res) {
     var quiz = models.Quiz.build(req.body.quiz);
 
-      quiz
-      .validate()
-      .then(
-            function(err){
-                  if (err) {
-                        res.render('quizes/new', {quiz: quiz, errors: err.errors});
-                      } else {
-                        quiz // save: guarda en DB campos pregunta y respuesta de quiz
-                        .save({fields: ["pregunta", "respuesta"]})
-                        .then( function(){ res.redirect('/quizes')})
-                      }      // res.redirect: Redirección HTTP a lista de preguntas
-                }
-          );
-
+    quiz
+        .validate()
+        .then(
+        function (err) {
+            if (err) {
+                res.render('quizes/new', {quiz: quiz, errors: err.errors});
+            } else {
+                quiz // save: guarda en DB campos pregunta y respuesta de quiz
+                    .save({fields: ["pregunta", "respuesta"]})
+                    .then(function () {
+                        res.redirect('/quizes')
+                    })
+            }      // res.redirect: Redirección HTTP a lista de preguntas
+        }
+        ).catch(function (error) {
+            next(error)
+        });
 };
 
 // GET /quizes/:id/edit
@@ -101,5 +104,16 @@ exports.update = function (req, res) {
                     });
             }     // Redirección HTTP a lista de preguntas (URL relativo)
         }
-    );
+        ).catch(function (error) {
+            next(error)
+        });
+};
+
+// DELETE /quizes/:id
+exports.destroy = function (req, res) {
+    req.quiz.destroy().then(function () {
+        res.redirect('/quizes');
+    }).catch(function (error) {
+        next(error)
+    });
 };
