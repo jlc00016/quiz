f36be858118f64c25275ebbf1970423d0185fd36
Jesús Lara <jlc00016@gmail.com>
2015-08-02 11:37:06 +0200

    Creación de comentarios para las preguntas

--- models/models.js
+++ models/models.js
@@ -23,14 +23,25 @@ var sequelize = new Sequelize(DB_name, user, pwd,
         port: port,
         host: host,
         storage: storage,  // solo SQLite (.env)
-        omitNull: true      // solo Postgres
+        omitNull: true     // solo Postgres
     }
 );
 
 // Importar la definicion de la tabla Quiz en quiz.js
-var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+var quiz_path = path.join(__dirname, 'quiz');
+var Quiz = sequelize.import(quiz_path);
 
-exports.Quiz = Quiz; // exportar definición de tabla Quiz
+// Importar definicion de la tabla Comment
+var comment_path = path.join(__dirname, 'comment');
+var Comment = sequelize.import(comment_path);
+
+// Relaciones entre tablas
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
+// exportar definiciones de tablas
+exports.Quiz = Quiz;
+exports.Comment = Comment;
 
 // sequelize.sync() inicializa tabla de preguntas en DB
 sequelize.sync().then(function () {
