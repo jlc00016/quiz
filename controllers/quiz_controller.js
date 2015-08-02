cd5c4360c31d0b7612ea23f245c234ec00747c29
Jesús Lara <jlc00016@gmail.com>
2015-08-02 10:57:49 +0200

    Añadir índice temático a las preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -67,7 +67,7 @@ exports.create = function (req, res) {
                 res.render('quizes/new', {quiz: quiz, errors: err.errors});
             } else {
                 quiz // save: guarda en DB campos pregunta y respuesta de quiz
-                    .save({fields: ["pregunta", "respuesta"]})
+                    .save({fields: ["pregunta", "respuesta", "tema"]})
                     .then(function () {
                         res.redirect('/quizes')
                     })
@@ -89,6 +89,7 @@ exports.edit = function (req, res) {
 exports.update = function (req, res) {
     req.quiz.pregunta = req.body.quiz.pregunta;
     req.quiz.respuesta = req.body.quiz.respuesta;
+    req.quiz.tema = req.body.quiz.tema;
 
     req.quiz
         .validate()
@@ -98,7 +99,7 @@ exports.update = function (req, res) {
                 res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
             } else {
                 req.quiz     // save: guarda campos pregunta y respuesta en DB
-                    .save({fields: ["pregunta", "respuesta"]})
+                    .save({fields: ["pregunta", "respuesta", "tema"]})
                     .then(function () {
                         res.redirect('/quizes');
                     });
--- models/models.js
+++ models/models.js
@@ -39,8 +39,8 @@ sequelize.sync().then(function () {
         if (count === 0) {   // la tabla se inicializa solo si está vacía
             Quiz.bulkCreate(
                 [
-                    { pregunta: 'Capital de Italia', respuesta: 'Roma' },
-                    { pregunta: 'Capital de Portugal', respuesta: 'Lisboa' }
+                    { pregunta: 'Capital de Italia', respuesta: 'Roma', tema: 'otros' },
+                    { pregunta: 'Capital de Portugal', respuesta: 'Lisboa', tema: 'otros' }
                 ]
             ).then(function () { console.log('Base de datos inicializada') });
         };
